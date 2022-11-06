9fe9fb40693bfe6cce78eaca96490cf4b0664396
DAN_casa <jazurduymontalvo@gmail.com>
2022-11-06 22:55:13 +0100

    Copia de BSS y cmabios en inscripcion

--- js/php.js
+++ js/php.js
@@ -1,5 +1,7 @@
-function validarcontacto() {
-    var y=document.forms["formulario"]["condiciones"].checked;
+const formulario=documentById('formulario');
+const inputs=document.querySelectorAll('#formulario input')
+function enablesend() {
+    document.eventos.submit.disabled=!document.loginform.terms.checked;
 
     if (y == false) {
         alert("Tienes que aceptar los términos y condiciones");
