cfecb5146d7aa2a36dca386e3a3156ec9214b954
DAN <ju.azurduy@aulanz.net>
2022-11-07 12:32:18 +0100

    cambios inscripcion

--- js/inscrib.js
+++ js/inscrib.js
@@ -1,11 +1,9 @@
 function validate(){
-    var name = document.getElementById("name").value;
-    var subject = document.getElementById("subject").value;
-    var phone = document.getElementById("phone").value;
+    var name = document.getElementById("name").value;  
     var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
+    var phone = document.getElementById("phone").value;
     var error_message = document.getElementById("error_message");
-    
+    var y=document.forms["formulario"]["condiciones"].checked;
     error_message.style.padding = "10px";
     
     var text;
@@ -14,11 +12,7 @@ function validate(){
       error_message.innerHTML = text;
       return false;
     }
-    if(subject.length < 10){
-      text = "Escribe un asunto correcto";
-      error_message.innerHTML = text;
-      return false;
-    }
+    
     if(isNaN(phone) || phone.length < 9){
       text = "Escribe un teléfono correcto";
       error_message.innerHTML = text;
@@ -29,11 +23,13 @@ function validate(){
       error_message.innerHTML = text;
       return false;
     }
-    if(message.length <= 140){
-      text = " Escribe Más de 140 carácteres ";
-      error_message.innerHTML = text;
-      return false;
+   
+
+    if (y == false) {
+      alert("Tienes que aceptar los términos y condiciones");
+        return false;
     }
+   
     alert("Se han guardado los datos correctamente!");
     return true;
   }
