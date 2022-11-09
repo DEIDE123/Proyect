743958e9dc2c323cbab24eaf0455064baf95f3b6
DAN <ju.azurduy@aulanz.net>
2022-11-09 13:37:21 +0100

    mal

--- js/terms.js
+++ js/terms.js
@@ -5,15 +5,20 @@ function validarFormulario() {
     var b=document.forms["eventos"]["opcion2"].checked;
     var c=document.forms["eventos"]["opcion3"].checked;
     var i=document.forms["eventos"]["opcion4"].checked;
-
+    var y=document.forms["eventos"]["terminos"].checked;
+    
     if ((a == false) && (b == false) && (c == false) && (i == false)){
         alert("Tienes que seleccionar un evento como mínimo");
         return false;
     }
-
-
-
-
+    
+    if (y == false) {
+        alert("Tienes que aceptar los términos y condiciones");
+         return false;
+    }
+    
+    
+    alert("Se han guardado los datos correctamente!");
         return true;
 
 
