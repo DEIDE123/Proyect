c45c4e991edbfc58e31e0ac66662cc4cae8f35dd
DAN_casa <jazurduymontalvo@gmail.com>
2022-11-09 22:50:19 +0100

    Js completed

--- js/email.js
+++ js/email.js
@@ -1,20 +1,30 @@
 function validarEmail(){
-var email = document.getElementById("email").value;
 
-/*var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+var email = document.getElementById("email").value;
+/*REGULAR EXPRESION
+/^([a-zA-Z0-9_\.\-])---> BEFORE THE '@' ONLY WORDS (A,Z) AND NUMBERS (0,9)
+\@---> MusT HAVE A '@'
+\@(([a-zA-Z0-9\-])--->AFTER THE '@' ONLY WORDS (A,Z) AND NUMBERS (0,9)
++\.---> MUST HAVE A '.'
+([a-zA-Z0-9]{2,4})+$/----> AFTER '.' ONLY WORD AND NUMBERS AND MUST TO HAVE A FOR 2,4 SIZE (IMPORT*->THE SIZE OF Extension doen´t work yet)
+*/
+
+var expReg= /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 var esValido=expReg.test(email);
 if(esValido==true){
     alert('El email es válido')
+    return true;
 }
 else{
-    alertalert('El email NO es válido')
-}
-*/
-if(email.indexOf("@") == -1 || email.length < 6){
-    alert(" Escribe un email correcto ");
-  
+    alert('El email NO es válido')
     return false;
 }
-alert("Se han guardado los datos correctamente!");
-  return true;
+
+//if(email.indexOf("@") == -1 || email.length < 6){
+    //alert(" Escribe un email correcto ");
+  
+  //  return false;
+//}
+//alert("Se han guardado los datos correctamente!");
+  //return true;
 }
--- js/terms.js
+++ js/terms.js
@@ -1,12 +1,32 @@
 function validarFormulario() {
+    var nombre = document.getElementById('nombre').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
  
-    var a=document.forms["formulario"]["opcion1"].checked;
+    var a=document.forms["formulario"]["opcion1"].checked;  //Terms and the events checkbox
     var b=document.forms["formulario"]["opcion2"].checked;
     var c=document.forms["formulario"]["opcion3"].checked;
     var i=document.forms["formulario"]["opcion4"].checked;
     var y=document.forms["formulario"]["terminos"].checked;
 
+    var expReg= /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; //Expresion regular for email
+    var esValido=expReg.test(email);
 
+
+
+    if(esValido==false){
+        alert('El email NO es válido')
+        return false;
+    }
+    if(isNaN(phone) || phone.length < 9){
+        alert('El telefono NO es válido')
+        return false;
+    }
+
+    if(nombre.length < 6){
+        alert('El nombre y apellido NO es válido')
+        return false;
+    }
     if ((a == false) && (b == false) && (c == false) && (i == false)){
         alert("Tienes que seleccionar un evento como mínimo");
         return false;
