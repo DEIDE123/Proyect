219d7dd42ad4e6cd5a63243189f9e06df424316e
DAN <ju.azurduy@aulanz.net>
2022-11-09 12:34:31 +0100

    sql

--- js/terms.js
+++ js/terms.js
@@ -1,13 +1,13 @@
 
-    function validarFormulario() {
-        var y=document.forms["eventos"]["terms"].checked;
+function validarFormulario() {
+    var y=document.forms["eventos"]["terms"].checked;
 
-        if (y == false) {
-            alert("Tienes que aceptar los términos y condiciones");
-            return false;
-        }
-        alert("Se han guardado los datos correctamente!");
-        return true;
+    if (y == false) {
+        alert("Tienes que aceptar los términos y condiciones");
+        return false;
     }
+    alert("Se han guardado los datos correctamente!");
+    return true;
+}
 
 
